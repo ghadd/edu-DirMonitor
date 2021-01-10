1bcd86afec27e190dc9d493bfeb85c3b3f8dfc36
Dan Timachov <danyatimachov@gmail.com>
2021-01-10 21:35:47 +0200

    Integrating errors with client's response

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include "src/server.h"
 
 int main() {
-    try {
-        Server server(9998);
-        server.setupThis();
+  try {
+    Server server(9999);
+    server.setupThis();
 
-        return server.run();
-    }  catch (const std::runtime_error& error) {
-        /*
-         * TODO EXCEPTION
-         */
-        throw;
-    }
+    return server.run();
+  } catch (const std::runtime_error &error) {
+    /*
+     * TODO EXCEPTION
+     */
+    throw;
+  }
 }
